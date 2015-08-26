2ae66eea8f4a03c440667b297a1018e62b52df2f
Elviss Strazdins <elviss@Elvisss-MacBook-Pro-2.local>
2015-08-26 11:43:25 +0300

    Initial Commit

---
+++ OpenCVTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCVTest
+//
+//  Created by Elviss Strazdins on 26/08/15.
+//  Copyright (c) 2015 Elviss Strazdins. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
